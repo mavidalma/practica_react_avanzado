5bf51485a14d19e1e3bd4c03d52cd3a23cae6f9e
mavidalma <mavidalma@gmail.com>
2020-05-27 21:28:50 +0200

    action test halfway

--- src/store/tests/actions.test.js
+++ src/store/tests/actions.test.js
@@ -1,7 +1,54 @@
 import * as TYPES from '../types';
+import * as action from '../actions';
 
 describe('check ads actions', ()=> {
-    test('should launch an API call', ()=>{
-        expect(true).toBe(true);
-    }
-)})
+    const ads = [
+        {
+            name: "test1",
+            description: "desription1",
+            price: 25,
+        },
+        {
+            name: "test2",
+            description: "desription2",
+            price: 10,
+        },
+    ];
+
+    const error = "505 error mocked"
+
+    test('fetchAdsSuccess should pass ads', ()=>{
+        const fakedAction = {
+            type:TYPES.FETCH_ADS_SUCCESS,
+            ads
+        }
+    expect(action.fetchAdsSuccess(ads)).toEqual(fakedAction);
+    });
+
+    test('fetchAdsFailure should pass error', ()=> {
+        const fakedAction = {
+            type:TYPES.FETCH_ADS_FAILURE,
+            error
+        }
+    expect(action.fetchAdsFailure(error)).toEqual(fakedAction);
+    expect(action.fetchAdsFailure(error).error).toEqual(error);
+    })
+
+    describe('fetchAds testing', ()=> {
+        let query = "";
+        const dispatch = jest.fn();
+        const getState = jest.fn();
+        const API = jest.fn()
+        beforeEach(()=> {
+            query = "";
+        })
+
+        test('fetchAds should dispatch request and success', async ()=> {
+            const fetch = action.fetchAds(query);
+            
+            await fetch(dispatch, getState);
+
+            expect(dispatch).toHaveBeenCalled();
+        })
+    })
+})
